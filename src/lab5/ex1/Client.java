fb40cb8efa797339dceb4ea208bfcc54e9586157
Admin <Admin@K-15>
2020-12-16 16:41:35 +0800

    Hibernate5 txManager

---
+++ src/lab5/ex1/Client.java
@@ -0,0 +1,30 @@
+package lab5.ex1;
+
+import lab5.ex1.Product;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"lab5/ex1/beans-config.xml");
+		ProductDao productDao = (ProductDao) context.getBean("productDao");
+        // add your code here
+		
+		Product product1 = productDao.getProduct(1);
+		product1.setName("Hibernate");
+		product1.setPrice(120);
+		productDao.save(product1);
+		
+		List<Product> list = productDao.getProducts();
+		for (Product p : list) {
+			System.out.println(p.getName());
+		}
+//		
+	}
+
+}
